672753827fd687478e18fed2c540a0ccf37fdaef
Rob <120913030+RoT1337@users.noreply.github.com>
2024-10-29 15:20:54 +0800

    Calculator App
    Designs and simple functionality. Will look into the logic of how to make the calculator

---
+++ Ionic/MyIonic/src/app/calculator/calculator.page.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInput } from '@ionic/angular';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.page.html',
+  styleUrls: ['./calculator.page.scss'],
+})
+export class CalculatorPage implements OnInit {
+  @ViewChild('input', { static: false }) inputElement!: IonInput;
+
+  currentInput: string = '';
+  previousInput: string = '';
+  operator: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onNumberClick(number: string) {
+    this.currentInput += number;
+    this.updateInputElement();
+  }
+
+  onArithmeticClick(symbol: string) {
+    if (this.currentInput === '') return;
+
+    if (this.previousInput !== '') {
+      this.calculateResult();
+    }
+
+    this.operator = symbol.trim();
+    this.previousInput = this.currentInput;
+    this.currentInput = '';
+  }
+
+  clearInput() {
+    this.currentInput = '';
+    this.previousInput = '';
+    this.operator = '';
+    this.updateInputElement();
+  }
+
+  calculateResult() {
+    let result: number;
+    const prev = parseFloat(this.previousInput);
+    const current = parseFloat(this.currentInput);
+
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (this.operator) {
+      case '+':
+        result = prev + current;
+        break;
+      case '-':
+        result = prev - current;
+        break;
+      case '*':
+        result = prev * current;
+        break;
+      case '/':
+        result = prev / current;
+        break;
+      case '%':
+        result = prev % current;
+        break;
+      default:
+        return;
+    }
+
+    this.currentInput = result.toString();
+    this.operator = '';
+    this.previousInput = '';
+    this.updateInputElement();
+  }
+
+  onDecimalClick() {
+    if (!this.currentInput.includes('.')) {
+      this.currentInput += '.';
+      this.updateInputElement();
+    }
+  }
+
+  onEqualsClick() {
+    this.calculateResult();
+  }
+
+  updateInputElement() {
+    this.inputElement.value = this.currentInput;
+  }
+}
